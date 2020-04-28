9f736d5ad9a5a838d161b0663dddfb3271f8046a
leylee <leylee2001@gmail.com>
2020-04-28 23:02:30 +0800

    Update list.h

--- list.h
+++ list.h
@@ -1,6 +1,22 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+#include "student.h"
+#include <stdlib.h>
 
+typedef struct _Node {
+    Student* stu;
+    struct _Node *lst, *nxt;
+} Node;
+
+typedef struct _List {
+    int length;
+    Node *head, *tail;
+} List;
+
+Node* newNode();
+void freeNode(Node* pt);
+List* newList();
+void freeList(List* pt);
 
 #endif // LIST_H_INCLUDED
